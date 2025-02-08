97cb8502ff453f152dcde860ef0a82b9c06c6be0
aimpert <aleximpert@gmail.com>
2025-02-07 17:20:58 -0800

    added frontend

---
+++ frontend/src/components/MainPage.vue
@@ -0,0 +1,70 @@
+<template>
+  Stuff:
+  <div>
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+      </li> 
+
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'MainPage',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      items: [],
+    };
+  },
+  mounted() {
+    this.fetchItems();
+  },
+  methods: {
+    async fetchItems() {
+      try {
+        const response = await axios.get(
+          'http://34.218.224.225:8000/test',
+          {
+            headers: {
+              'Access-Control-Allow-Origin': '*',
+              'Content-Type': 'application/json',
+            }
+          }
+        );
+        //const response = await axios.get('http://34.218.224.225:8000/test');
+        this.items = response.data;
+      }
+      catch (error) {
+        console.error("Failed to fetch items:", error);
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
