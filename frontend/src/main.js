910239d639e16ecbbea0c44f2eff8e36750c98d5
aimpert <aleximpert@gmail.com>
2025-02-10 22:45:59 -0800

    actual working code

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -17,8 +17,8 @@ import Bump from "./assets/Bump.jpg"
 import Clouds from "./assets/Clouds.png"
 import Ocean from "./assets/Ocean.png"
 import NightLights from "./assets/night_lights_modified.png"
-import vertexShader from "./shaders/vertex.glsl"
-import fragmentShader from "./shaders/fragment.glsl"
+// import vertexShader from "./shaders/vertex.glsl"
+// import fragmentShader from "./shaders/fragment.glsl"
 import GaiaSky from "./assets/Gaia_EDR3_darkened.png"
 
 global.THREE = THREE
@@ -83,25 +83,16 @@ let app = {
     // loads earth's color map, the basis of how our earth looks like
     const albedoMap = await loadTexture(Albedo)
     albedoMap.colorSpace = THREE.SRGBColorSpace
-    await updateLoadingProgressBar(0.2)
 
     const bumpMap = await loadTexture(Bump)
-    await updateLoadingProgressBar(0.3)
+
     
     const cloudsMap = await loadTexture(Clouds)
-    await updateLoadingProgressBar(0.4)
+
 
     const oceanMap = await loadTexture(Ocean)
-    await updateLoadingProgressBar(0.5)
 
-    const lightsMap = await loadTexture(NightLights)
-    await updateLoadingProgressBar(0.6)
 
-    const envMap = await loadTexture(GaiaSky)
-    envMap.mapping = THREE.EquirectangularReflectionMapping
-    await updateLoadingProgressBar(0.7)
-    
-    scene.background = envMap
 
     // create group for easier manipulation of objects(ie later with clouds and atmosphere added)
     this.group = new THREE.Group()
@@ -116,8 +107,6 @@ let app = {
       roughnessMap: oceanMap, // will get reversed in the shaders
       metalness: params.metalness, // gets multiplied with the texture values from metalness map
       metalnessMap: oceanMap,
-      emissiveMap: lightsMap,
-      emissive: new THREE.Color(0xffff88),
     })
     this.earth = new THREE.Mesh(earthGeo, earthMat)
     this.group.add(this.earth)
@@ -134,30 +123,8 @@ let app = {
     this.earth.rotateY(-0.3)
     this.clouds.rotateY(-0.3)
 
-    let atmosGeo = new THREE.SphereGeometry(12.5, 64, 64)
-    let atmosMat = new THREE.ShaderMaterial({
-      vertexShader: vertexShader,
-      fragmentShader: fragmentShader,
-      uniforms: {
-        atmOpacity: params.atmOpacity,
-        atmPowFactor: params.atmPowFactor,
-        atmMultiplier: params.atmMultiplier
-      },
-      // notice that by default, Three.js uses NormalBlending, where if your opacity of the output color gets lower, the displayed color might get whiter
-      blending: THREE.AdditiveBlending, // works better than setting transparent: true, because it avoids a weird dark edge around the earth
-      side: THREE.BackSide // such that it does not overlays on top of the earth; this points the normal in opposite direction in vertex shader
-    })
-    this.atmos = new THREE.Mesh(atmosGeo, atmosMat)
-    this.group.add(this.atmos)
-
     scene.add(this.group)
 
-    // meshphysical.glsl.js is the shader used by MeshStandardMaterial: https://github.com/mrdoob/three.js/blob/dev/src/renderers/shaders/ShaderLib/meshphysical.glsl.js
-    // shadowing of clouds, from https://discourse.threejs.org/t/how-to-cast-shadows-from-an-outer-sphere-to-an-inner-sphere/53732/6
-    // some notes of the negative light map done on the earth material to simulate shadows casted by clouds
-    // we need uv_xOffset so as to act as a means to calibrate the offset of the clouds shadows on earth(especially when earth and cloud rotate at different speeds)
-    // the way I need to use fracts here is to get a correct calculated result of the cloud texture offset as it moves,
-    // arrived at current method by doing the enumeration of cases (writing them down truly helps, don't keep everything in your head!)
     earthMat.onBeforeCompile = function( shader ) {
       shader.uniforms.tClouds = { value: cloudsMap }
       shader.uniforms.tClouds.value.wrapS = THREE.RepeatWrapping;
@@ -181,57 +148,7 @@ let app = {
 
         #endif
       `);
-      shader.fragmentShader = shader.fragmentShader.replace('#include <emissivemap_fragment>', `
-        #ifdef USE_EMISSIVEMAP
-
-          vec4 emissiveColor = texture2D( emissiveMap, vEmissiveMapUv );
-
-          // Methodology of showing night lights only:
-          //
-          // going through the shader calculations in the meshphysical shader chunks (mostly on the vertex side),
-          // we can confirm that geometryNormal is the normalized normal in view space,
-          // for the night side of the earth, the dot product between geometryNormal and the directional light would be negative
-          // since the direction vector actually points from target to position of the DirectionalLight,
-          // for lit side of the earth, the reverse happens thus emissiveColor would be multiplied with 0.
-          // The smoothstep is to smoothen the change between night and day
-          
-          emissiveColor *= 1.0 - smoothstep(-0.02, 0.0, dot(geometryNormal, directionalLights[0].direction));
-          
-          totalEmissiveRadiance *= emissiveColor.rgb;
-
-        #endif
-
-        // Methodology explanation:
-        //
-        // Our goal here is to use a “negative light map” approach to cast cloud shadows,
-        // the idea is on any uv point on earth map(Point X),
-        // we find the corresponding uv point(Point Y) on clouds map that is directly above Point X,
-        // then we extract color value at Point Y.
-        // We then darken the color value at Point X depending on the color value at Point Y,
-        // that is the intensity of the clouds at Point Y.
-        //
-        // Since the clouds are made to spin twice as fast as the earth,
-        // in order to get the correct shadows(clouds) position in this earth's fragment shader
-        // we need to minus earth's UV.x coordinate by uv_xOffset,
-        // which is calculated and explained in the updateScene()
-        // after minus by uv_xOffset, the result would be in the range of -1 to 1,
-        // we need to set RepeatWrapping for wrapS of the clouds texture so that texture2D still works for -1 to 0
-
-        float cloudsMapValue = texture2D(tClouds, vec2(vMapUv.x - uv_xOffset, vMapUv.y)).r;
-        
-        // The shadow should be more intense where the clouds are more intense,
-        // thus we do 1.0 minus cloudsMapValue to obtain the shadowValue, which is multiplied to diffuseColor
-        // we also clamp the shadowValue to a minimum of 0.2 so it doesn't get too dark
-        
-        diffuseColor.rgb *= max(1.0 - cloudsMapValue, 0.2 );
-
-        // adding small amount of atmospheric coloring to make it more realistic
-        // fine tune the first constant for stronger or weaker effect
-        float intensity = 1.4 - dot( geometryNormal, vec3( 0.0, 0.0, 1.0 ) );
-        vec3 atmosphere = vec3( 0.3, 0.6, 1.0 ) * pow(intensity, 5.0);
-        diffuseColor.rgb += atmosphere;
-      `)
-
+    
       // need save to userData.shader in order to enable our code to update values in the shader uniforms,
       // reference from https://github.com/mrdoob/three.js/blob/master/examples/webgl_materials_modified.html
       earthMat.userData.shader = shader
@@ -242,14 +159,11 @@ let app = {
     gui.add(params, "sunIntensity", 0.0, 5.0, 0.1).onChange((val) => {
       this.dirLight.intensity = val
     }).name("Sun Intensity")
-    gui.add(params, "metalness", 0.0, 1.0, 0.05).onChange((val) => {
-      earthMat.metalness = val
-    }).name("Ocean Metalness")
+    // gui.add(params, "metalness", 0.0, 1.0, 0.05).onChange((val) => {
+    //   earthMat.metalness = val
+    // }).name("Ocean Metalness")
     gui.add(params, "speedFactor", 0.1, 20.0, 0.1).name("Rotation Speed")
-    gui.add(params.atmOpacity, "value", 0.0, 1.0, 0.05).name("atmOpacity")
-    gui.add(params.atmPowFactor, "value", 0.0, 20.0, 0.1).name("atmPowFactor")
-    gui.add(params.atmMultiplier, "value", 0.0, 20.0, 0.1).name("atmMultiplier")
-
+    
     // Stats - show fps
     this.stats1 = new Stats()
     this.stats1.showPanel(0) // Panel 0 = fps
